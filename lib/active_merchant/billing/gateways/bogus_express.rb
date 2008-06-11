34d59a02e8ee62d28471b20f88a9c7aaebf6a224
Greg Furmanek <greg@furmanek.net>
2008-06-10 22:16:23 -0700

    Updated bogus_express

--- lib/active_merchant/billing/gateways/bogus_express.rb
+++ lib/active_merchant/billing/gateways/bogus_express.rb
@@ -1,15 +1,56 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
+    class BogusExpressResponse < Response
+      def email
+        "greg_fu@example.com"
+      end
+      
+      def name
+        "Greg Fu"
+      end
+      
+      def token
+        "1234567890"
+      end
+      
+      def payer_id
+        "123"
+      end
+      
+      def payer_country
+        "US"
+      end
+      
+      def address
+        {  'name'       => "Greg", 
+           'company'    => "Fu",
+           'address1'   => "123 Test St",
+           'address2'   => "",
+           'city'       => "Test City",
+           'state'      => "AZ",
+           'country'    => "US",
+           'zip'        => 85001,
+           'phone'      => nil
+        }
+      end
+    end
+
     # Bogus Gateway
     class BogusExpressGateway < Gateway
       AUTHORIZATION = '53433'
+
+
       LIVE_REDIRECT_URL = 'https://www.paypal.com/cgibin/webscr?cmd=_express-checkout&token='
       TEST_REDIRECT_URL = 'https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token='
 
+      CONFIRM_TOKEN = "CONFIRM"
+      CANCEL_TOKEN  = "CANCEL"
+
       
-      SUCCESS_MESSAGE = "Bogus Express Gateway: Forced success"
+      SUCCESS_MESSAGE = "Bogus Express Gateway Success"
       FAILURE_MESSAGE = "Bogus Express Gateway: Forced failure"
-      ERROR_MESSAGE = "Bogus Express Gateway: Use CreditCard number 1 for success, 2 for exception and anything else for error"
+      ERROR_MESSAGE = "Bogus Express Gateway: Error message"
+
       CREDIT_ERROR_MESSAGE = "Bogus Express Gateway: Use trans_id 1 for success, 2 for exception and anything else for error"
       UNSTORE_ERROR_MESSAGE = "Bogus Express Gateway: Use trans_id 1 for success, 2 for exception and anything else for error"
       CAPTURE_ERROR_MESSAGE = "Bogus Express Gateway: Use authorization number 1 for exception, 2 for error and anything else for success"
@@ -20,26 +61,39 @@ module ActiveMerchant #:nodoc:
       self.display_name = 'Bogus Express'
 
       def redirect_url
-        test? ? TEST_REDIRECT_URL : LIVE_REDIRECT_URL 
+
       end
       
-      def redirect_url_for(token)
-        "#{redirect_url}#{token}"
+      def redirect_url_for(token, options = {})
+        unless options[:cancel]
+          ENV["success_url"] % [token, 1]
+        else
+          ENV["cancel_url"] % [token, 1]
+        end
       end
 
       def setup_authorization(money, options={})
         requires!(options, :return_url, :cancel_return_url)
-        PaypalExpressResponse.new(
-          true, 
-          SUCCESS_MESSAGE,
-          self.response_hash(options),
-          { :test => true, }.update(options)
-        )
+        if money != 14000
+          BogusExpressResponse.new(
+            true, 
+            SUCCESS_MESSAGE,
+            self.response_hash(options),
+            { :test => true, }.update(options)
+          )
+        else
+          BogusExpressResponse.new(
+            false, 
+            FAILURE_MESSAGE,
+            self.response_hash(options),
+            { :test => true, }.update(options)
+          )
+        end
       end
 
       def setup_purchase(money, options = {})
         requires!(options, :return_url, :cancel_return_url)
-        PaypalExpressResponse.new(
+        BogusExpressResponse.new(
           true, 
           SUCCESS_MESSAGE,
           response_hash(options),
@@ -48,18 +102,18 @@ module ActiveMerchant #:nodoc:
       end
 
       def details_for(token)
-        PaypalExpressResponse.new(
+        BogusExpressResponse.new(
           true, 
           SUCCESS_MESSAGE,
-          response_hash(options),
-          { :test => true, }.update(options)
+          response_hash,
+          { :test => true, }
         )
       end
 
 
       def authorize(money, options = {})
         requires!(options, :token, :payer_id)
-        PaypalExpressResponse.new(
+        BogusExpressResponse.new(
           true, 
           SUCCESS_MESSAGE,
           response_hash(options),
@@ -69,7 +123,7 @@ module ActiveMerchant #:nodoc:
 
       def purchase(money, options = {})
         requires!(options, :token, :payer_id)
-        PaypalExpressResponse.new(
+        BogusExpressResponse.new(
           true, 
           SUCCESS_MESSAGE,
           response_hash(options),
@@ -79,13 +133,14 @@ module ActiveMerchant #:nodoc:
 
       def capture(money, authorization, options = {})
         requires!(options, :token, :payer_id)
-        PaypalExpressResponse.new(
+        BogusExpressResponse.new(
           true, 
           SUCCESS_MESSAGE,
           response_hash(options),
           { :test => true, }.update(options)
         )
       end
+
       def response_hash(options = {})
         {
           :payer => "john@doe.com", 
